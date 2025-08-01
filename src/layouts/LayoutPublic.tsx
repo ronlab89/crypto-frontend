b2502813724d18279ea3a5af93a34f539b9cde96
Ronald Labrador <ronlabdev@gmail.com>
2025-08-01 17:12:09 -0500

    create toggle theme button and configure dark theme in tailwind CSS

--- src/layouts/LayoutPublic.tsx
+++ src/layouts/LayoutPublic.tsx
@@ -1,9 +1,13 @@
+import ToggleTheme from "@/components/ui/ToggleTheme";
 import { Outlet } from "react-router-dom";
 
 const LayoutPublic = () => {
   return (
     <main className="min-w-screen max-w-screen min-h-screen max-h-screen overflow-hidden bg-crypto-light dark:bg-crypto-dark text-crypto-dark dark:text-crypto-light font-crypto-body">
-      <header>navbar</header>
+      <header>
+        navbar
+        <ToggleTheme position="absolute" right="0" top="0" />
+      </header>
       <Outlet />
     </main>
   );
