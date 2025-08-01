6ef76dfe4e2da20fa479c55ad6d2a69519854e34
Ronald Labrador <ronlabdev@gmail.com>
2025-08-01 18:45:09 -0500

    create auth store and improve navbar design

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,6 +1,9 @@
 import { useToggleStore } from "@/store/toggle.store";
+import { useAuthStore } from "@/store/auth.store";
+
 import ToggleTheme from "@/components/ui/ToggleTheme";
 import UserDropdown from "@/components/navigation/UserDropdown";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const toggleDropdownUser = useToggleStore(
@@ -9,37 +12,55 @@ const Navbar = () => {
   const setToggleDropdownUser = useToggleStore(
     (state) => state.setToggleDropdownUser
   );
+  const userLogged = useAuthStore((state) => state.userLogged);
+  const token = useAuthStore((state) => state.token);
+
   return (
     <header className="antialiased">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center">
           <div className="flex justify-start items-center">
-            <a href="/" className="flex mr-4">
+            <NavLink to={"/"} className="flex mr-4">
               <img src="/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-crypto-light font-crypto-title">
                 Crypto Investment
               </span>
-            </a>
+            </NavLink>
           </div>
-          <div className="flex items-center lg:order-2 relative">
+          <div className="flex items-center gap-4 lg:order-2 relative">
             <ToggleTheme position="relative" right="0" top="0" />
-            <span className="block">Login</span>
 
-            <button
-              type="button"
-              className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="dropdown"
-              onClick={() => setToggleDropdownUser(!toggleDropdownUser)}
-            >
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="w-8 h-8 rounded-full"
-                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                alt="user photo"
-              />
-            </button>
+            {!userLogged && !token ? (
+              <div className="flex items-center gap-4">
+                <NavLink to={"/login"}>
+                  <span className="block text-sm dark:bg-crypto-light bg-crypto-dark dark:text-crypto-dark text-crypto-light font-medium px-3 py-1 rounded-[.5rem] cursor-pointer">
+                    Iniciar sesión
+                  </span>
+                </NavLink>
+                <NavLink to={"/register"}>
+                  <span className="block text-sm bg-crypto-yellow text-crypto-dark font-medium px-3 py-1 rounded-[.5rem] cursor-pointer">
+                    Registrarse
+                  </span>
+                </NavLink>
+              </div>
+            ) : (
+              <button
+                type="button"
+                className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                id="user-menu-button"
+                aria-expanded="false"
+                data-dropdown-toggle="dropdown"
+                onClick={() => setToggleDropdownUser(!toggleDropdownUser)}
+              >
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="w-8 h-8 rounded-full"
+                  src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                  alt="user photo"
+                />
+              </button>
+            )}
+
             {/* <!-- Dropdown menu --> */}
             <UserDropdown />
           </div>
---
+++ src/store/auth.store.ts
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+// Tipado general del store
+export type User = {
+  name: string;
+  lastName: string;
+  email: string;
+  rol: string;
+  createdAt: string;
+};
+
+interface AuthState {
+  token: string | null;
+  logged: boolean;
+  userLogged: User | null;
+
+  // Métodos
+  setToken: (data: string | null) => void;
+  setLogged: (bool: boolean) => void;
+  setUserLogged: (user: User) => void;
+  resetAuth: () => void;
+}
+
+const initialState = {
+  token: null,
+  logged: false,
+  userLogged: null,
+};
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      ...initialState,
+
+      setToken: (data) => set({ token: data }),
+      setLogged: (bool) => set({ logged: bool }),
+      setUserLogged: (user) => set({ userLogged: user }),
+
+      resetAuth: () => set(initialState),
+    }),
+    {
+      name: "auth",
+      partialize: (state) => ({
+        logged: state.logged,
+        userLogged: state.userLogged,
+      }),
+      // onRehydrateStorage: () => (state) => {
+      //   // console.log("Rehydrating auth state...", state);
+      // },
+    }
+  )
+);
